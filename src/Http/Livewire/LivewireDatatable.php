48581a15a642be8c2fb371494c0dcc7af6206124
Chris George <cgeorge@tektone.com>
2023-02-06 09:58:33 -0500

    Trying renardudezert query builder fixes

--- src/Http/Livewire/LivewireDatatable.php
+++ src/Http/Livewire/LivewireDatatable.php
@@ -19,6 +19,8 @@ use Mediconesystems\LivewireDatatables\Exports\DatatableExport;
 use Mediconesystems\LivewireDatatables\Traits\WithCallbacks;
 use Mediconesystems\LivewireDatatables\Traits\WithPresetDateFilters;
 use Mediconesystems\LivewireDatatables\Traits\WithPresetTimeFilters;
+//renardudezert fix
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class LivewireDatatable extends Component
 {
@@ -921,6 +923,8 @@ class LivewireDatatable extends Component
     {
         if (isset($key)) {
             unset($this->activeTextFilters[$column][$key]);
+
+            
             if (count($this->activeTextFilters[$column]) < 1) {
                 unset($this->activeTextFilters[$column]);
             }
@@ -1040,8 +1044,20 @@ class LivewireDatatable extends Component
     {
         $this->row = 1;
 
+        //renardudezert fix
+        $perPage = ($this->getQuery()->limit && ($this->getQuery()->limit < $this->perPage)) ? $this->getQuery()->limit : $this->perPage;
+        $paginatedQuery = $this->getQuery()->paginate($perPage);
+        $total = ($this->getQuery()->limit) ?: $paginatedQuery->total();
+        $paginatedQuery = new LengthAwarePaginator(
+            $paginatedQuery->items(),
+            $paginatedQuery->total() < $total ? $paginatedQuery->total() : $total,
+            $this->perPage,
+            $paginatedQuery->currentPage(),
+            $paginatedQuery->getOptions()
+        );
+
         return $this->mapCallbacks(
-            $this->getQuery()->paginate($this->perPage)
+            $paginatedQuery
         );
     }
 
@@ -1644,14 +1660,16 @@ class LivewireDatatable extends Component
 
     public function checkboxQuery()
     {
-        return $this->query->reorder()->get()->map(function ($row) {
+        //renardudezert fix
+        return $this->query->get()->map(function ($row) {
             return (string) $row->checkbox_attribute;
         });
     }
 
     public function toggleSelectAll()
     {
-        if (count($this->selected) === $this->getQuery()->getCountForPagination()) {
+        //renardudezert fix
+        if (count($this->selected) === ($this->getQuery()->limit ?: $this->getQuery()->getCountForPagination())) {
             $this->selected = [];
         } else {
             $this->selected = $this->checkboxQuery()->values()->toArray();
